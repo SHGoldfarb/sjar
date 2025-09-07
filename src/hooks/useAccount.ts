f7dd173f62b4b8f1695a5ee9a8ff17a6ab40df83
Samuel <samuelghau@gmail.com>
2025-09-06 23:27:44 -0400

    fix: fix not memoing getaccount and getjar

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,13 +1,17 @@
 import { useAsync } from "@/hooks/useAsync";
 import { dbGetAccount } from "@/lib/database/accounts";
 import { useAccountsStaleIndicator } from "@/providers/AccountsStaleIndicator";
+import { useMemo } from "react";
 
 export const useAccount = (id: number) => {
   const [staleIndicator] = useAccountsStaleIndicator();
 
-  const { data, isLoading } = useAsync(() => dbGetAccount(id), {
-    deps: [staleIndicator],
-  });
+  const { data, isLoading } = useAsync(
+    useMemo(() => () => dbGetAccount(id), [id]),
+    {
+      deps: [staleIndicator],
+    }
+  );
 
   return { data, isLoading };
 };
--- src/hooks/useJar.ts
+++ src/hooks/useJar.ts
@@ -1,13 +1,17 @@
 import { useAsync } from "@/hooks/useAsync";
 import { dbGetJar } from "@/lib/database/jars";
 import { useJarsStaleIndicator } from "@/providers/JarsStaleIndicator";
+import { useMemo } from "react";
 
 export const useJar = (id: number) => {
   const [staleIndicator] = useJarsStaleIndicator();
 
-  const { data, isLoading } = useAsync(() => dbGetJar(id), {
-    deps: [staleIndicator],
-  });
+  const { data, isLoading } = useAsync(
+    useMemo(() => () => dbGetJar(id), [id]),
+    {
+      deps: [staleIndicator],
+    }
+  );
 
   return { data, isLoading };
 };
